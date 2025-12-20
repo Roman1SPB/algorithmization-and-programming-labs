0db9ef73509e21351c6ceb15fca6db58f99ee23e
Roman1SPB <romanus.slepushkin@gmail.com>
2025-12-20 20:39:14 +0300

    boss lab solved(100%)

--- 6/6.c
+++ 6/6.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define MAX_LENGTH_AUTHOR_NAME 60
+#define MAX_LENGTH_BOOK_AUTHOR 60
 #define MAX_LENGTH_BOOK_TITLE 60
 #define MAX_LENGTH_BOOK_CATEGORY 60
-#define MAX_TOTAL_LENGTH (MAX_LENGTH_AUTHOR_NAME+MAX_LENGTH_BOOK_TITLE+MAX_LENGTH_BOOK_CATEGORY+32)
+#define TOTAL_BOOK_ATTRIBUTES_LENGTH (MAX_LENGTH_BOOK_AUTHOR+MAX_LENGTH_BOOK_TITLE+MAX_LENGTH_BOOK_CATEGORY+32)
 
 int library_max_size = 5;
 int library_size = 0;
 
 struct Book {
-    char author_name[MAX_LENGTH_AUTHOR_NAME];
+    char author[MAX_LENGTH_BOOK_AUTHOR];
     char title[MAX_LENGTH_BOOK_TITLE];
     int year;
     int price;
-    // char category[MAX_LENGTH_BOOK_CATEGORY];
+    char category[MAX_LENGTH_BOOK_CATEGORY];
 };
 
 struct Book* library;
 
-
+int check_input(int choice, int handle_input_error, int range_from, int range_to) {
+    if (handle_input_error == 0 || choice < 1 || choice > 7) {
+        printf("There is input mistake\n");
+        fflush(stdin);
+        return 0;
+    } else return 1;
+}
 // case 1: Just printing all book titles
 void print_library() {
     if (library_size == 0) {
@@ -29,8 +36,9 @@ void print_library() {
 
     for (int i = 0; i < library_size; i++) {
         printf("Book %d:\n", i+1);
-        printf("Author name: %s\n", library[i].author_name);
+        printf("Book author: %s\n", library[i].author);
         printf("Book title: %s\n", library[i].title);
+        printf("Book category: %s\n", library[i].category);
         printf("Year published: %d\n", library[i].year);
         printf("Book price: %d\n", library[i].price);
         printf("\n");
@@ -48,7 +56,6 @@ void handle_input_strings(const int LIMIT, char strng[]) {
     }
 }
 
-
 void check_library_size() {
     if (library_size >= library_max_size) {
         library_max_size *= 2;
@@ -60,15 +67,21 @@ void check_library_size() {
 void scan_new_book() {
     check_library_size();
 
-    printf_s("Write author name\n");
-    handle_input_strings(MAX_LENGTH_AUTHOR_NAME, library[library_size].author_name);
+    printf_s("Write author\n");
+    handle_input_strings(MAX_LENGTH_BOOK_AUTHOR, library[library_size].author);
     printf_s("Write title\n");
     handle_input_strings(MAX_LENGTH_BOOK_TITLE, library[library_size].title);
+    printf_s("Write category\n");
+    handle_input_strings(MAX_LENGTH_BOOK_CATEGORY, library[library_size].category);
     printf("Write year\n");
-    scanf_s("%d", &library[library_size].year);
+    int check_input1_error = scanf_s("%d", &library[library_size].year);
     printf("Write price\n");
-    scanf_s("%d", &library[library_size].price);
-    library_size++;
+    int check_input2_error = scanf_s("%d", &library[library_size].price);
+
+    int check_input1_result = check_input(library[library_size].year, check_input1_error, -10000, 10000);
+    int check_input2_result = check_input(library[library_size].year, check_input2_error, -10000, 10000);
+    if (check_input1_result == 0 || check_input2_result == 0) return scan_new_book();
+    else library_size++;
 }
 
 // Compare strings function from previous labs
@@ -103,14 +116,6 @@ int find_book_in_library(char book_title[]) {
     return -1;
 }
 
-// Just swap to field(for example titles)
-void swap_books_fields(char array1[], char array2[], int limit) {
-    for (int i = 0; i < limit; i++) {
-        array1[i] = array2[i];
-        if (array1[i] == '\0') break;
-    }
-}
-
 // case 3: Delete book from library
 // Actually, we just replace the last 
 // book fields to book, which we want to delete and then decresse
@@ -128,13 +133,8 @@ void delete_book() {
     int index_book_to_delete = find_book_in_library(book_title);
     if (index_book_to_delete == -1) printf("Title book not founded!\n");
     else {
-        if (library_size != 1) {
-            swap_books_fields(library[index_book_to_delete].author_name, library[library_size-1].author_name, MAX_LENGTH_AUTHOR_NAME);
-            swap_books_fields(library[index_book_to_delete].title, library[library_size-1].title, MAX_LENGTH_BOOK_TITLE);
-            library[index_book_to_delete].price = library[library_size-1].price;
-            library[index_book_to_delete].year = library[library_size-1].price;
-        }
-
+        if (library_size != 1) library[index_book_to_delete] = library[library_size-1];
+        printf("Book successfully deleted!\n");
         library_size--;
     }
 
@@ -146,9 +146,10 @@ void write_library_to_drive() {
     fptr = fopen("library_output.txt", "w");
 
     for (int i = 0; i < library_size; i++) {
-        fprintf(fptr, "%s\n%s\n%d\n%d\n", library[i].author_name, library[i].title, library[i].year, library[i].price);
+        fprintf(fptr, "%s\n%s\n%d\n%d\n%d\n", library[i].author, library[i].title, library[i].year, library[i].price, library[i].category);
     }
     fclose(fptr);
+    printf("Library successfully saved to 'library_output.txt' file\n");
 }  
 
 void write_to_library_field(int start, int end, char buff[], char field[]) {
@@ -165,19 +166,25 @@ void change_last_char_to_null_char(char buff[]) {
     buff[i] = '\0';
 }
 
-// case 5: get books from file(library_input.txt) to ram
+// case 5: get books from file(library_input.txt) to library
 void write_from_drive_to_library() {
     FILE *fptr;
     fptr = fopen("library_input.txt", "r");
+    if (fptr == NULL) {
+        printf("You should create file 'library_input.txt' and place books there\n");
+        return;
+    }
 
     int library_size_copy = library_size;
     for (int i = library_size_copy; !feof(fptr); i++) {
-        fgets(library[i].author_name, MAX_LENGTH_AUTHOR_NAME, fptr);
-        change_last_char_to_null_char(library[i].author_name);
+        fgets(library[i].author, MAX_LENGTH_BOOK_AUTHOR, fptr);
+        change_last_char_to_null_char(library[i].author);
         fgets(library[i].title, MAX_LENGTH_BOOK_TITLE, fptr);
         change_last_char_to_null_char(library[i].title);
         fscanf(fptr, "%d\n", &library[i].year);
         fscanf(fptr, "%d\n", &library[i].price);
+        fgets(library[i].category, MAX_LENGTH_BOOK_CATEGORY, fptr);
+        change_last_char_to_null_char(library[i].title);
 
         library_size++;
         check_library_size();
@@ -186,22 +193,99 @@ void write_from_drive_to_library() {
     fclose(fptr);
 }
 
-
-int check_menu_input(int action, int handle_input_error) {
-    if (handle_input_error == 0 || action < 1 || action > 6) {
-        printf("There is input mistake\n");
-        return 0;
-    } else return 1;
-}
-
 void print_menu_actions() {
     printf("1 - Print library titles\n");
     printf("2 - Scan new book\n");
     printf("3 - Delete book\n");
     printf("4 - Write library to drive\n");
     printf("5 - Write from file\n");
-    printf("6 - Exit\n");
+    printf("6 - Sort library\n");
+    printf("7 - Exit\n");
 }
+
+void swap_books(struct Book* book1, struct Book* book2) {
+    struct Book temp = *book1;
+    *book1 = *book2;
+    *book2 = temp;
+}
+
+short cmp_author(struct Book* book1, struct Book* book2) {
+    int cmp_result = compare_strings(book1->author, book2->author);
+    return (cmp_result == 1) ? 0 : 1;
+}
+short cmp_title(struct Book* book1, struct Book* book2) {
+    int cmp_result = compare_strings(book1->title, book2->title);
+    return (cmp_result == 1) ? 0 : 1;
+}
+
+short cmp_price(struct Book* book1, struct Book* book2) {
+    return (book1->price < book2->price) ? 0 : 1;
+}
+
+short cmp_year(struct Book* book1, struct Book* book2) {
+    return (book1->year <= book2->year) ? 0 : 1;
+}
+
+short cmp_category(struct Book* book1, struct Book* book2) {
+    int cmp_result = compare_strings(book1->category, book2->category);
+    return (cmp_result == 1) ? 0 : 1;
+}
+
+void print_criteria() {
+    printf("1 - Sort by author\n");
+    printf("2 - Sort by title\n");
+    printf("3 - Sort by catergory\n");
+    printf("3 - Sort by year\n");
+    printf("4 - Sort by price\n\n");
+    
+    printf("1 - Increase order\n");
+    printf("2 - Decrease order\n");
+}
+
+void sort_books(short (*cmp)(struct Book* book1, struct Book* book2), int order) {
+    // order: 1 - increae, 2 - decrease 
+    for (int i = 0; i < library_size; i++) {
+        struct Book* criteria_book = &library[i];
+        for (int j = i + 1; j < library_size; j++) {
+            short cmp_result = cmp(&library[j], criteria_book);
+            if (cmp_result == 0 && order == 1) criteria_book = &library[j];
+            if (cmp_result == 1 && order == 2) criteria_book = &library[j];
+        }
+        
+        swap_books(&library[i], criteria_book);
+    }
+}
+
+void sort_criteria() {
+    print_criteria();
+    int criteria, order;
+    int handle_input1_error = scanf("%d", &criteria);
+    int handle_input2_error = scanf("%d", &order);
+    int check_input1_result = check_input(criteria, handle_input1_error, 1, 5);
+    int check_input2_result = check_input(order, handle_input2_error, 1, 2);
+    if (check_input1_result == 0 || check_input2_result == 0) return sort_criteria();
+
+    switch (criteria) {
+        case 1:
+            sort_books(cmp_author, order);
+            break;
+        case 2:
+            sort_books(cmp_title, order);
+            break;
+        case 3:
+            sort_books(cmp_category, order);
+            break;
+        case 4:
+            sort_books(cmp_year, order);
+            break;
+        case 5:
+            sort_books(cmp_price, order);
+            break;
+        default:
+            printf("switch error???76\n");
+            break;
+    }
+} 
 
 // Menu actions with our library
 void menu() {
@@ -209,7 +293,7 @@ void menu() {
     int action;
     int handle_input_error = scanf_s("%d", &action);
     
-    int check_menu_result = check_menu_input(action, handle_input_error);
+    int check_menu_result = check_input(action, handle_input_error, 1, 7);
     if (check_menu_result == 0) return menu();
 
     char ch; scanf("%c", &ch); // Last useless charachter '\n'
@@ -230,6 +314,9 @@ void menu() {
             write_from_drive_to_library();
             break;
         case 6:
+            sort_criteria();
+            break;
+        case 7:
             return;
         default:
             printf("switch error???67\n");
